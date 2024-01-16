ebe5f50ca8e3a4ea6989da9a5f5092e0545e0827
RamonaVcn <98316083+RamonaVcn@users.noreply.github.com>
2024-01-16 18:11:02 +0200

    Update ingest_data_csv.py

--- ingest_data_csv.py
+++ ingest_data_csv.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from elasticsearch import Elasticsearch
 
-csv_file_path = 'popular_movies.csv'
+# Replace with the path to your CSV file
+csv_file_path = 'popular_movies.csv' 
 username = 'elastic'
-password = ''
+password = 'c9NGxWa76m74ALoQD2V3T843'
+es = Elasticsearch('https://8c965989446641d19f0ba4fd20db082b.us-central1.gcp.cloud.es.io:9243', basic_auth=(username, password))
 df = pd.read_csv(csv_file_path)
 df.dropna(inplace=True)
-es = Elasticsearch('https://elasticsearchapp.es.northeurope.azure.elastic-cloud.com:9243', basic_auth=(username, password))
 
 index_name = 'movies_index'
-
-
 for index, row in df.iterrows():
     row_dict = row.to_dict()
-    es.index(index=index_name, id=index, document=row_dict)
+    es.index(index=index_name, id=index, body=row_dict)
+    
